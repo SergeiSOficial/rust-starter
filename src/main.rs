4fa6fa27a4306c6f512af97abcf48ce533a764d6
Sergei Savkin <geos2@mail.ru>
2020-10-02 10:39:51 +0300

    started listen com port

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
-extern crate foo;
+extern crate serialport;
 
-use module3::module4;
-use foo::foo as foo_;
-use foo::Bar;
+use serialport::prelude::*;
+use std::io::{self};
+use std::time::Duration;
 
-#[macro_use]
-mod macros;
-
-mod module1;
-mod module2;
-mod module3;
+static PREAMBULA: &'static [u8] = &[0x01, 0x02, 0x02, 0x02];
 
 fn main() {
-  module4::blah::doit();
-  foo_();
-  let _ = Bar;
+  let s = SerialPortSettings {
+    baud_rate: 115200,
+    data_bits: DataBits::Eight,
+    flow_control: FlowControl::None,
+    parity: Parity::None,
+    stop_bits: StopBits::One,
+    timeout: Duration::from_millis(1000),
+  };
+  let _port = serialport::open_with_settings("COM5", &s);
+  match _port {
+    Ok(mut _port) => {
+      println!("Ready:");
+      let mut serial_buf: Vec<u8> = vec![0; 1000];
+      println!("Receiving data:");
+      loop {
+        match _port.read(serial_buf.as_mut_slice()) {
+          Ok(t) => process_data(&serial_buf[..t]), //io::stdout().write_all(&serial_buf[..t]).unwrap(),
+          Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+          Err(e) => eprintln!("{:?}", e),
+        }
+      }
+    }
+    Err(_e) => {
+      eprintln!("Failed to open. Error:{} ", _e);
+
+      ::std::process::exit(1);
+    }
+  }
+}
+
+fn process_data(n: &[u8]) -> () {
+  if n[0..7].eq(PREAMBULA) {
+    let preamb = &n[..7];
+    println!("Receiving data:{:?}", preamb);
+  } else {
+    println!("None found");
+  }
 }
